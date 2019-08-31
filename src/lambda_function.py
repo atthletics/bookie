e017ff2e3d3fa8beee152eb2302e841196ab1694
healyt22 <healyt22@gmail.com>
2019-08-31 09:19:12 -0400

    os winner and game status added

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,23 +1,21 @@
 import os, yaml
 from webdriver_wrapper import WebDriverWrapper
-import espn_spreads
-import os_spreads
+import es_data
+import os_data
 fp = os.path.dirname(os.path.realpath(__file__))
 
 def lambda_handler(*args, **kwargs):
     with open(fp + '/config.yaml', 'r') as f:
         config = yaml.load(f)
 
-    #ESPN
     driver = WebDriverWrapper()
     driver.get_url(config['url1'])
     driver.get_soup()
-    espn_spreads.Parse(driver.soup)
+    es_data.ProcessGamesToS3(driver.soup, 1)
     driver.close()
 
-    #Oddsshark
     driver = WebDriverWrapper()
     driver.get_url(config['url2'])
     driver.get_soup()
-    os_spreads.Parse(driver.soup)
+    os_data.ProcessGamesToS3(driver.soup, 1)
     driver.close()
